7693445e0fc69480a30617cb7bdfea8562aa8dff
NatalComp <gone.for.swing@gmail.com>
2023-11-25 18:01:35 +0400

    structure

---
+++ src/main/java/project/valery/SoloRandomizer/SoloRandomizerApplication.java
@@ -0,0 +1,13 @@
+package project.valery.SoloRandomizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloRandomizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloRandomizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/valery/SoloRandomizer/SoloRandomizerApplicationTests.java
@@ -0,0 +1,13 @@
+package project.valery.SoloRandomizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloRandomizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
